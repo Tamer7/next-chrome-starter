0a12b3aa3e0a9384afbc4bfe5b526a818641e59e
tamerjar <algarmakany.ext@nubedian.de>
2023-11-04 23:00:22 +0200

    Side bar

--- components/Index/index.js
+++ components/Index/index.js
@@ -1,18 +1,13 @@
-import styles from '../../styles/Pages.module.css';
+// components/Index/index.js
+import React from 'react';
+import PageWithSidebar from '../PageWithSidebar';
+// ... import other components and styles
 
 export default function Index({ navigateToPage }) {
+  // Your index page content
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>NEXT-CHROME-STARTER</h1>
-        <p className={styles.description}>
-          This is an example of a Browser Extension built with NEXT.JS. Please
-          refer to the GitHub repo for running instructions and documentation
-        </p>
-        <h1 className={styles.code}>Index Page ./components/Index/index.js</h1>
-        <p>{"[ - This is Index page content - ]"}</p>
-        <p onClick={() => navigateToPage('new')}>{"Go to New Page >"}</p>
-      </main>
-    </div>
+    <PageWithSidebar>
+
+    </PageWithSidebar>
   );
 }
